b916541e6fed39990347963a1ddf285b0cae0268
Xiaofei Ren <xiaofei.ren.js@gmail.com>
2017-05-31 21:25:11 -0700

    Remove projectId messagingSenderId from env variables since they are not required

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -5,9 +5,7 @@ try {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
+    storageBucket: process.env.STORAGE_BUCKET
   };
   firebase.initializeApp(config);
 } catch (error) {
--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,7 @@ module.exports = {
         API_KEY: JSON.stringify(process.env.API_KEY),
         AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
         DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-        PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
-        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
-        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
       }
     })
   ],
